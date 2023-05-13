302724b21148f59c77b62d4cba9e1e9469472a3d
Q <sunflower@Qs-MacBook-Air.local>
2023-05-13 11:19:00 -0700

    MVC Routing

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const hbs = require('hbs');
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -13,7 +14,7 @@ var app = express();
 // View engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
 
-// register handlebars partials (https://www.npmjs.com/package/hbs) 
+// Register handlebars partials (https://www.npmjs.com/package/hbs) 
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials')); 
 
 app.set('view engine', 'hbs');
