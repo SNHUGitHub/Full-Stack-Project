641aadf848f3f15e7c94ff5056f94a526d53bb8a
Q <sunflower@Qs-MacBook-Air.local>
2023-05-27 18:59:58 -0700

    Create object schema with Mongoose & populate MongoDB

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -3,7 +3,7 @@ const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => { 
-    pageTitle = process.env.npm_package_description + ' - Travel';
+    pageTitle = 'Travlr Getaways - Travel';
     res.render('travel', {title: pageTitle, trips});
 }; 
 
