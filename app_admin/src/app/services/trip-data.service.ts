a4ebeda486cdd387ecc5ed1d0a210c3f7df0dbba
Q <sunflower@Qs-MacBook-Air.local>
2023-06-20 23:30:50 -0700

    Added Single-Page Application with Angular

---
+++ app_admin/src/app/services/trip-data.service.ts
@@ -0,0 +1,51 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+
+import { Trip } from '../models/trip';
+
+@Injectable()
+export class TripDataService {
+  constructor(private http: Http) { }
+
+  private apiBaseUrl = "http://localhost:3000/api/";
+
+  public getTrips(): Promise<Trip[]> {
+    console.log("Inside TripDataService#getTrips");
+    return this.http
+      .get(`${this.apiBaseUrl}trips`)
+      .toPromise()
+      .then(response => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  /*public addTrip(formData: Trip): Promise<Trip> {
+    console.log("Inside TripDataService#addTrip");
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
+    return this.http
+      .post(this.tripUrl, formData, { headers: headers })
+      .toPromise()
+      .then((response) => response.json() as Trip[])
+      .catch(this.handleError);
+  }
+
+  public updateTrip(formData: Trip): Promise<Trip> {
+    console.log("Inside TripDataService#updateTrip");
+    const headers = new Headers({
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${localStorage.getItem("travlr-token")}`,
+    });
+    return this.http
+      .put(this.tripUrl + formData.code, formData, { headers: headers })
+      .toPromise()
+      .then((response) => response.json() as Trip[])
+      .catch(this.handleError);
+  }*/
+
+  private handleError(error: any): Promise<any> {
+    console.error("Something has gone wrong", error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+}
